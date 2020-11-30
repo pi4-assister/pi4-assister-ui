dc20226623140e6a516d85a59855c4a99c2e712a
Alessandro Ciambarella <alessandro.ciambarella@nextsoft.com.br>
2020-11-30 16:32:43 -0300

    payments

--- src/components/profile/ProfilePayments.vue
+++ src/components/profile/ProfilePayments.vue
@@ -3,42 +3,38 @@
     <div class="rounded shadow p-4">
       <div class="d-flex align-items-center justify-content-between">
         <h5 class="mb-0">Formas de Pagamento:</h5>
-        <button type="button" data-toggle="modal" data-target="#addnewcard" class="btn btn-primary">
+        <!--<button type="button" class="btn btn-primary">
           Adicionar
-        </button>
+        </button>-->
       </div>
       <div class="row">
         <div class="col-md-6 mt-4 pt-2">
-          <a href="javascript:void(0)">
-            <div class="card rounded shadow bg-light border-0">
-              <div class="card-body">
-                <img :src="Master" height="60" class="text-right" alt="">
-                <div class="mt-4">
-                  <h5 class="text-dark">•••• •••• •••• 4559</h5>
-                  <div class="d-flex justify-content-between">
-                    <p class="h6 text-muted mb-0">Cristino Murfy</p>
-                    <h6 class="mb-0 text-dark">Exp: <span class="text-muted">10/22</span></h6>
-                  </div>
+          <div class="card rounded shadow bg-light border-0">
+            <div class="card-body">
+              <img :src="Master" height="60" class="text-right" alt="">
+              <div class="mt-4">
+                <h5 class="text-dark">•••• •••• •••• 4559</h5>
+                <div class="d-flex justify-content-between">
+                  <p class="h6 text-muted mb-0">{{ user.fullName }}</p>
+                  <h6 class="mb-0 text-dark">Exp: <span class="text-muted">10/22</span></h6>
                 </div>
               </div>
             </div>
-          </a>
+          </div>
         </div>
         <div class="col-md-6 mt-4 pt-2">
-          <a href="javascript:void(0)">
-            <div class="card rounded shadow bg-dark border-0">
-              <div class="card-body">
-                <img :src="Visa" height="60" class="text-right" alt="">
-                <div class="mt-4">
-                  <h5 class="text-light">•••• •••• •••• 9856</h5>
-                  <div class="d-flex justify-content-between">
-                    <p class="h6 text-muted mb-0">Calvin Carlo</p>
-                    <h6 class="mb-0 text-muted">Exp: <span class="text-muted">01/24</span></h6>
-                  </div>
+          <div class="card rounded shadow bg-dark border-0">
+            <div class="card-body">
+              <img :src="Visa" height="60" class="text-right" alt="">
+              <div class="mt-4">
+                <h5 class="text-light">•••• •••• •••• 9856</h5>
+                <div class="d-flex justify-content-between">
+                  <p class="h6 text-muted mb-0">{{ user.fullName }}</p>
+                  <h6 class="mb-0 text-muted">Exp: <span class="text-muted">01/24</span></h6>
                 </div>
               </div>
             </div>
-          </a>
+          </div>
         </div>
       </div>
     </div>
@@ -55,6 +51,11 @@ export default {
     Master,
     Visa,
   }),
+  computed: {
+    user() {
+      return this.$store.state.auth.user;
+    },
+  },
 };
 </script>
 
